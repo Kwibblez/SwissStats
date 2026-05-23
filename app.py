dfaf36d1c414b3c03063e41d1912dcf6770420b1
Daniella Smith <125796560+Kwibblez@users.noreply.github.com>
2026-05-23 15:07:20 +0200

    remove _ that made everything crash

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def api_stats():
         "vaches":         round(totals["total_m"] / 2.4),
         "lac_leman":      round(total_km / 170, 1),
         "cervin":     round(totals["total_elevation"] / 4478 , 1),
-        "swissTotal": (round(total_km / 66 000, 2))*100,
+        "swissTotal": (round(total_km / 66000, 2))*100,
         "fondues":         round(totals["total_calories"] / 800),
         "heures":         round(totals["total_seconds"] / 3600, 1),
     }
